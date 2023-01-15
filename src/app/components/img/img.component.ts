42b00455b2a2e5edec4e4c50bc2766952322769f
jorge niglia <jorge.niglia@gmail.com>
2023-01-15 14:47:45 -0300

    hasta inputs

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent {
+  @Input() img: string = 'initial value'
+}
